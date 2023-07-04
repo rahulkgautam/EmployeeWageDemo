87fbd52f8641cf0d3b43e68e3f8eaeb4cab4c104
rahulkgautam <131359869+rahulkgautam@users.noreply.github.com>
2023-07-04 08:56:26 +0530

    Update CheckEmployeeAttendance.cs

--- EmployeeWageProblem/CheckEmployeeAttendance.cs
+++ EmployeeWageProblem/CheckEmployeeAttendance.cs
@@ -8,12 +8,12 @@ namespace EmployeeWageProblem
     {
         public static void EmployeeAttendance()
         {
-            int IS_FULL_TIME = 1;
+            int IS_PRESENT = 1;
             Random random = new Random();
             int empCheck = random.Next(2);
-            if (empCheck == IS_FULL_TIME)            
-                Console.WriteLine("Employee is Present");            
-            else            
+            if (empCheck == IS_PRESENT)
+                Console.WriteLine("Employee is Present");
+            else
                 Console.WriteLine("Employee is Absent");            
         }
     }
